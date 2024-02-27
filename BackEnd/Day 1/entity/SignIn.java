e13123655c4c90b11b658d7debb004903ea5fbdb
DHAKSHINYA-C-A <118709379+DHAKSHINYA-C-A@users.noreply.github.com>
2024-02-27 16:28:17 +0530

    Add files via upload

---
+++ BackEnd/Day 1/entity/SignIn.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.GenerationType;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="signin")
+public class SignIn
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name" ,nullable=false)
+    private String name;
+
+    @Column(name="email" ,nullable=false)
+    private String email;
+
+    @Column(name="password" ,nullable=false)
+    private String password;
+}
