01a166cc5952153bb80849f8058d91c3d8e02722
DHAKSHINYA-C-A <118709379+DHAKSHINYA-C-A@users.noreply.github.com>
2024-02-29 16:18:14 +0530

    Delete BackEnd/Day 2 directory

--- BackEnd/Day 2/src/main/java/com/example/demo/service/FeedbackService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-import java.util.List;
-import com.example.demo.dto.FeedBackDTO;
-
-public interface FeedbackService {
-     List<FeedBackDTO> getAllFeedBack();
-
-}
--- BackEnd/Day 2/src/main/java/com/example/demo/service/ProfileService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-import java.util.List;
-import com.example.demo.dto.ProfileDTO;
-public interface ProfileService {
-    List<ProfileDTO> getAllProfile();
-
-    ProfileDTO updateProfile(Long profileId, ProfileDTO updatedPofileDto);
-}
